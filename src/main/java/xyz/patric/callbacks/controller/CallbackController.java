c20bb4ff887ccfd994811e0c496fa286d9fdb184
Patrick Mutwiri <patwiri@gmail.com>
2024-06-29 20:13:30 +0300

    Callbacks fetching from DB: Done. Ready for tests?
    Signed-off-by: Patrick Mutwiri <patwiri@gmail.com>

--- src/main/java/xyz/patric/callbacks/controller/CallbackController.java
+++ src/main/java/xyz/patric/callbacks/controller/CallbackController.java
@@ -1,15 +1,41 @@
 /*
  * Copyright (c) 2024.
- * @author Patrick Mutwiri <dev@patric.xyz> on 6/29/24, 1:16 PM
+ * @author Patrick Mutwiri <dev@patric.xyz> on 6/29/24, 8:13 PM
  *
  */
 
 package xyz.patric.callbacks.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import xyz.patric.callbacks.entity.Callback;
+import xyz.patric.callbacks.model.CallbackMdl;
+import xyz.patric.callbacks.service.CallbackService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/callbacks")
 public class CallbackController {
+    private final CallbackService callbackService;
+
+    @Autowired
+    public CallbackController(CallbackService callbackService) {
+        this.callbackService = callbackService;
+    }
+
+    @GetMapping("/{id}")
+    public Callback getCallbackById(@PathVariable Long id) {
+        return callbackService.getCallbackById(id);
+    }
+
+    @GetMapping("/{path}")
+    public List<Callback> getCallbacksByPath(@PathVariable String path) {
+        return callbackService.getCallbacksByPath(path);
+    }
+
+    @PostMapping("/{path}")
+    public Callback createCallback(@PathVariable String path, @RequestBody CallbackMdl callback) {
+        return callbackService.saveCallback(path, callback);
+    }
 }
--- src/main/java/xyz/patric/callbacks/service/CallbackService.java
+++ src/main/java/xyz/patric/callbacks/service/CallbackService.java
@@ -1,12 +1,47 @@
 /*
  * Copyright (c) 2024.
- * @author Patrick Mutwiri <dev@patric.xyz> on 6/29/24, 5:30 PM
+ * @author Patrick Mutwiri <dev@patric.xyz> on 6/29/24, 8:13 PM
  *
  */
 
 package xyz.patric.callbacks.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import xyz.patric.callbacks.entity.Callback;
+import xyz.patric.callbacks.entity.Route;
+import xyz.patric.callbacks.model.CallbackMdl;
+import xyz.patric.callbacks.repository.CallbackRepository;
+import xyz.patric.callbacks.repository.RouteRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
-public class CallbackService {}
+public class CallbackService {
+    private final RouteRepository routeRepository;
+    private final CallbackRepository callbackRepository;
+
+    @Autowired
+    public CallbackService(CallbackRepository callbackRepository, RouteRepository routeRepository) {
+        this.callbackRepository = callbackRepository;
+        this.routeRepository = routeRepository;
+    }
+
+    public Callback getCallbackById(Long id) {
+        return callbackRepository.findById(id).orElse(null);
+    }
+
+    public List<Callback> getCallbacksByPath(String path) {
+        return new ArrayList<>(callbackRepository.findAllByRoute(routeRepository.findByPath(path)));
+    }
+
+    public Callback saveCallback(String path, CallbackMdl callback) {
+        Route route = routeRepository.findByPath(path);
+        Callback c = new Callback();
+        c.setRoute(route);
+        c.setType(c.getType());
+        c.setPayload(c.getPayload());
+        return callbackRepository.saveAndFlush(c);
+    }
+}
