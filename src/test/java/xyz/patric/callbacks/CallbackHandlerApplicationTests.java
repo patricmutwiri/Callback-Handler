41d5841cb5cb08234639b536949f35bf2f29bb7d
Patrick Mutwiri <patwiri@gmail.com>
2024-06-29 21:59:32 +0300

    tests
    Signed-off-by: Patrick Mutwiri <patwiri@gmail.com>

--- src/test/java/xyz/patric/callbacks/CallbackHandlerApplicationTests.java
+++ src/test/java/xyz/patric/callbacks/CallbackHandlerApplicationTests.java
@@ -1,13 +1,27 @@
+/*
+ * Copyright (c) 2024.
+ * @author Patrick Mutwiri <dev@patric.xyz> on 6/29/24, 9:59 PM
+ *
+ */
+
 package xyz.patric.callbacks;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+
+import static org.springframework.test.util.AssertionErrors.assertNotNull;
 
 @SpringBootTest
 class CallbackHandlerApplicationTests {
 
+    @Autowired
+    private Environment environment;
+
 	@Test
 	void contextLoads() {
+		String name = environment.getProperty("spring.application.name");
+		assertNotNull(name, "Application name has been fetched!");
 	}
-
 }
